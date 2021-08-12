974f9339872e6e57ab06c78ad26d69294185e2a9
Esa Firman <esafirm21@gmail.com>
2021-08-12 16:14:37 +0700

    fix no-op api

---
+++ library-no-op/src/main/java/com/chuckerteam/chucker/api/extramodule/ChuckerExtraModuleRegistry.kt
@@ -0,0 +1,7 @@
+package com.chuckerteam.chucker.api.extramodule
+
+public object ChuckerExtraModuleRegistry {
+    public fun addExtraModule(extraModule: ChuckerExtraModule) {
+        // NO-OP
+    }
+}
