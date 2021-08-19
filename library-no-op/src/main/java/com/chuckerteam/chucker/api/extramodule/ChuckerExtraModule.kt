a808373b7f2cb899af9b66094a932733e497d63f
Esa Firman <esafirm21@gmail.com>
2021-08-19 19:47:39 +0700

    fix mismatched API

--- library-no-op/src/main/java/com/chuckerteam/chucker/api/extramodule/ChuckerExtraModule.kt
+++ library-no-op/src/main/java/com/chuckerteam/chucker/api/extramodule/ChuckerExtraModule.kt
@@ -2,7 +2,7 @@ package com.chuckerteam.chucker.api.extramodule
 
 import android.content.Context
 
-public interface ExtraModule {
+public interface ChuckerExtraModule {
     public val moduleName: String
     public fun onNavigateToModule(context: Context)
 }
