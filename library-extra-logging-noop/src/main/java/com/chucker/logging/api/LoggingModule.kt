60af2cbd1f8978b65407883fa6ffe9ed7d489cb9
Michael <michael.michael@tiket.com>
2021-09-15 17:46:17 +0700

    fix resrouce name

--- library-extra-logging-noop/src/main/java/com/chucker/logging/api/LoggingModule.kt
+++ library-extra-logging-noop/src/main/java/com/chucker/logging/api/LoggingModule.kt
@@ -5,10 +5,10 @@ import android.content.Context
 import com.chuckerteam.chucker.api.extramodule.ChuckerExtraModule
 
 class LoggingModule(
-    app: Application,
+    app: Context,
 ) : ChuckerExtraModule {
 
-    override val moduleName: String = "Tracker Log"
+    override val moduleName: String = "Log Logging"
 
     override fun onNavigateToModule(context: Context) {
         // noop
--- library-extra-logging/src/main/java/com/chucker/logging/api/LoggingModule.kt
+++ library-extra-logging/src/main/java/com/chucker/logging/api/LoggingModule.kt
@@ -1,13 +1,12 @@
 package com.chucker.logging.api
 
-import android.app.Application
 import android.content.Context
-import com.chucker.logging.internal.ui.LoggingActivity
 import com.chucker.logging.internal.data.repository.LoggingRepositoryProvider
+import com.chucker.logging.internal.ui.LoggingActivity
 import com.chuckerteam.chucker.api.extramodule.ChuckerExtraModule
 
 class LoggingModule(
-    app: Application
+    app: Context
 ) : ChuckerExtraModule {
 
     override val moduleName: String = "Tracker Log"
